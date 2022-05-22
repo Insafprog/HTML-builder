79de1cb1781ea41ebcc82fd91994c6722210ecb2
Insafprog <insaf@gmx.ru>
2022-05-22 10:19:37 +0300

    feat: 02-write-file complite

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+
+const rl = readline.createInterface({input:process.stdin, output:process.stdout})
+
+const filename = path.join(__dirname, '/text.txt')
+
+const writeStream = fs.createWriteStream(filename, {encoding: 'utf-8'})
+
+rl.write('Введите сообщение:\n')
+rl.on('line', (input) => {
+    if(input == 'exit') rl.close()
+    writeStream.write(input + '\n')
+})
+
+rl.on('close', () => {
+    console.log('Спасибо за проверку!')
+});02-
