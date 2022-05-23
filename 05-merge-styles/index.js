cbf778983c33b4b65fca49d86a694c52f95aea19
Insafprog <insaf@gmx.ru>
2022-05-23 13:03:49 +0300

    feat: 05-merge-styles complete

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir_from = path.join(__dirname, 'styles');
+const path_to = path.join(__dirname, 'project-dist/bundle.css');
+
+(async () => {
+
+    const files = await fs.promises.readdir(dir_from, { withFileTypes: true })
+    const writeStream = fs.createWriteStream(path_to, {encoding: 'utf-8'})
+
+    for (let e of files) {
+        if (e.isFile()) {
+            const name_arr = e.name.split('.')
+            const ext = name_arr.pop()
+            const filename = path.join(dir_from, e.name)
+
+            if(ext == "css") {
+                const readStream = fs.createReadStream(filename, {encoding: 'utf-8'})
+                readStream.on('readable', () => {
+                    const data = readStream.read()
+                    if (data != null) {
+                        writeStream.write(data)
+                    }
+                })
+            }
+        }
+    }
+})()
